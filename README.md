88aa66c497e9a7e02f366d44b0444b40366ec8da
Murph Murphy <murph@clurictec.com>
2023-12-15 17:50:50 +0800

    Add IronCore Labs Cloaked AI encryption

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Cloaked AI + Marqo
 
+This is an example of using Marqo AI together with IronCore Labs Cloaked AI to query over encrypted AI embeddings.
+
+## Functionality
+
+Indexes two encrypted documents into Marqo and queries them out. The documents, query, encrypted versions of each, and search results are all printed.
+
+## Usage
+
+Wait for the "welcome to marqo" message from `marqo-1` before running the python script.
+
 ```
 docker compose up
 pmd run cloaked-marqo.py
 ```
-
-## Current State
-
-Still trying to get Marqo's getting started example running. The DIND expected setup in the getting started doesn't seem to work with NixOS rootless docker. I created a `docker-compose.yaml` based on their [M1 Mac](https://docs.marqo.ai/1.5.0/Guides/m1_mac_users/) workaround where DIND also doesn't work. That doesn't work yet, there's an SSL issue with the `marqo-os` container, it fails its healthcheck so the `marqo` container never tries to start.
 
--- cloaked-marqo.py
+++ cloaked-marqo.py
@@ -1,28 +1,144 @@
+import asyncio
 import marqo
 import pprint
+import ironcore_alloy
+import time
 
-mq = marqo.Client(url="http://localhost:8882")
 
-mq.create_index("my-first-index", model="hf/all_datasets_v4_MiniLM-L6")
+def head_context(list):
+    return f"[{list[0]}, {list[1]}, {list[2]}, {list[3]}, {list[4]}, ..]"
 
-mq.index("my-first-index").add_documents(
-    [
-        {
-            "Title": "The Travels of Marco Polo",
-            "Description": "A 13th-century travelogue describing Polo's travels",
+
+async def main() -> None:
+    # sample OpenAI ada-002 embeddings generated from the nfcorpus dataset of documents and queries
+    # can be found [here](https://github.com/skeet70/beir-openai-embeddings/) if you'd like "real"
+    # data.
+    # replace with your own embedding dimensionality
+    dimensions = 768
+
+    # replace with your own document embeddings
+    doc1 = [-0.1234, 0.5678, 0.9012, -0.3456] * 192  # must match index dimensionality
+    doc2 = [-0.4321, 0.8765, 0.2222, -0.3333] * 192  # must match index dimensionality
+
+    # replace with your own query embedding
+    query = [-0.3333, 0.5555, 0.9021, -0.3556] * 192  # must match index dimensionality
+
+    # print out a few values of the plaintext vectors
+    print(f"doc1 {head_context(doc1)}")
+    print(f"doc2 {head_context(doc2)}")
+    print(f"query {head_context(query)}")
+
+    ##
+    ## Encrypt our document and query embeddings with IronCore Labs Cloaked AI
+    ##
+    encryption_secret_key = ironcore_alloy.Secret(
+        "awholelotoftotallyrandomdatathatcanbeusedasasecurecryptokey".encode("utf-8")
+    )
+    standalone_mode_secret = ironcore_alloy.StandaloneSecret(1, encryption_secret_key)
+    # encryption keys can be rotated
+    rotatable_secret = ironcore_alloy.RotatableSecret(standalone_mode_secret, None)
+    # this can be adjusted to achieve a balance of security and search accuracy
+    approximation_factor = 2.0
+    # used when encrypting embedding vectors
+    vector_secrets = {
+        "secret_path": ironcore_alloy.VectorSecret(
+            approximation_factor, rotatable_secret
+        )
+    }
+    # standard secrets, used if you want to store encrypted data alongside your vectors,
+    # not used in this example
+    standard_secrets = ironcore_alloy.StandardSecrets(None, [])
+    ironcore_config = ironcore_alloy.StandaloneConfiguration(
+        standard_secrets,
+        # deterministic secrets, used if you want encrypted data alongside your vectors
+        # that can be exact matched on, not used in this example
+        {},
+        vector_secrets,
+    )
+    ironcore_sdk = ironcore_alloy.Standalone(ironcore_config)
+    metadata = ironcore_alloy.AlloyMetadata.new_simple("")
+
+    # encrypt the vectors
+    encrypted_doc1 = (
+        await ironcore_sdk.vector().encrypt(
+            ironcore_alloy.PlaintextVector(doc1, "secret_path", ""), metadata
+        )
+    ).encrypted_vector
+    encrypted_doc2 = (
+        await ironcore_sdk.vector().encrypt(
+            ironcore_alloy.PlaintextVector(doc2, "secret_path", ""), metadata
+        )
+    ).encrypted_vector
+    encrypted_query = (
+        await ironcore_sdk.vector().generate_query_vectors(
+            {"query": ironcore_alloy.PlaintextVector(query, "secret_path", "")},
+            metadata,
+        )
+    )["query"][0].encrypted_vector
+
+    # print out a few values of the encrypted vectors
+    print(f"doc1 {head_context(encrypted_doc1)}")
+    print(f"doc2 {head_context(encrypted_doc2)}")
+    print(f"query {head_context(encrypted_query)}")
+
+    ##
+    ## Ingest the encrypted documents into Marqo AI and query with the encrypted query
+    ##
+    mq = marqo.Client(url="http://localhost:8882")
+
+    mq.index("my-first-index").delete()
+
+    index_settings = {
+        "index_defaults": {
+            "model": "no_model",
+            "model_properties": {"dimensions": dimensions},
         },
-        {
-            "Title": "Extravehicular Mobility Unit (EMU)",
-            "Description": "The EMU is a spacesuit that provides environmental protection, "
-            "mobility, life support, and communications for astronauts",
-            "_id": "article_591",
+    }
+
+    mq.create_index("my-first-index", settings_dict=index_settings)
+
+    # Define any raw (encrypted) embedding fields here
+    my_mappings = {
+        "my_custom_encrypted_vector": {"type": "custom_vector"},
+    }
+
+    mq.index("my-first-index").add_documents(
+        documents=[
+            {
+                "_id": "doc1",
+                "my_custom_encrypted_vector": {
+                    "vector": encrypted_doc1,
+                    "content": "encrypted document",
+                },
+            },
+            {
+                "_id": "doc2",
+                "my_custom_encrypted_vector": {
+                    "vector": encrypted_doc2,
+                    "content": "encrypted document",
+                },
+            },
+        ],
+        tensor_fields=["my_custom_encrypted_vector"],
+        mappings=my_mappings,
+    )
+
+    # wait for opensearch indexing to take place
+    time.sleep(5)
+
+    results = mq.index("my-first-index").search(
+        context={
+            "tensor": [
+                {
+                    "vector": encrypted_query,
+                    "weight": 1,
+                }
+            ]
         },
-    ],
-    tensor_fields=["Title", "Description"],
-)
+    )
 
-results = mq.index("my-first-index").search(
-    q="What is the best outfit to wear on the moon?"
-)
+    pprint.pprint(results)
 
-pprint.pprint(results)
+
+if __name__ == "__main__":
+    asyncio.run(main())
